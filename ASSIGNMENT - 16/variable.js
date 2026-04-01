5240c799c0715f12a2c887868e6b261db76dc5c3
dhushitha1359 <161479400+dhushitha1359@users.noreply.github.com>
2026-04-01 18:51:10 +0530

    Add files via upload

---
+++ ASSIGNMENT - 16/variable.js
@@ -0,0 +1,53 @@
+var name ="DHUSHITHA";
+console.log(name)
+
+let age = 8;
+if (age>=18){
+    console.log("eligible")
+}
+else{
+    console.log("not eligible")
+}
+
+
+const isGraduating = true;
+
+console.log(isGraduating);
+
+
+
+
+
+
+
+
+
+let word = "welcome";
+let number = 3986094;
+let isbool = false;
+let emptyValue = null;
+let notAssigned;
+
+console.log(
+    "String:", word,
+    "Number:", number,
+    "Boolean:", isbool,
+    "Null:", emptyValue,
+    "Undefined:", notAssigned
+);
+
+let a = 10;
+let b = 3;
+console.log(a + b);  
+console.log(a - b);  
+console.log(a * b);  
+console.log(a / b);  
+console.log(a % b); 
+console.log(a == "10");   
+console.log(a === "10");  
+
+let x = true;
+let y = false;
+
+console.log(x && y);  
+console.log(x || y);  
