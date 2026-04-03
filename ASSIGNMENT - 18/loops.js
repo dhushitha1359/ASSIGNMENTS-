2840c1ef9ff3cff9a9e70ea2ca935273993d7190
dhushitha1359 <161479400+dhushitha1359@users.noreply.github.com>
2026-04-03 22:03:13 +0530

    Add files via upload

---
+++ ASSIGNMENT - 18/loops.js
@@ -0,0 +1,30 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let i = 1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+
+let i=1;
+do{
+    console.log(i);
+    i++;
+}
+    while(i<=3);
+
+
+for (let i=1;i<=10;i++){
+    if(i==5){
+        continue;
+    }
+
+    if(i==8){
+        break;
+    }
+    console.log(i)
+}
+    
