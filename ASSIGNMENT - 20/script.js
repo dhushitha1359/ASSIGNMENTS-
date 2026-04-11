6e792ad78e910d91ecdd7c836c7fa2a946cad814
dhushitha1359 <161479400+dhushitha1359@users.noreply.github.com>
2026-04-11 09:03:53 +0530

    Add files via upload

---
+++ ASSIGNMENT - 20/script.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,4,5];
+arr.push(6);
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+arr.shift()
+console.log(arr);
+
+arr.unshift(0)
+console.log(arr);
+
+let arr1 = [10, 20, 30, 40];
+
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+}
+
