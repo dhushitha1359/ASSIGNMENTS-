f3b27e20b949b1cb2abb9a474146abc346402c75
dhushitha1359 <161479400+dhushitha1359@users.noreply.github.com>
2026-04-11 09:59:05 +0530

    Add files via upload

---
+++ ASSIGNMENT - 21/script.js
@@ -0,0 +1,31 @@
+const number = [2,3,4,5];
+const newArr = number.map(myFunction)
+function myFunction(num){
+    return num*2;
+
+}
+console.log(newArr);
+
+const number1 = [1,2,3,4,5,6,7,8,9,10];
+const newArr1 = number1.filter(fill)
+function fill(num1){
+    return num1 % 2 ==0;
+}
+console.log(newArr1)
+
+let arr6 = [1, 2, 3, 4];
+
+let sum = arr6.reduce(function(total, num) {
+    return total + num;
+}, 0);
+
+console.log(sum); 
+
+let arr5 = [5, 8, 12, 20, 3];
+
+let result = arr5.find(function(num) {
+    return num > 10;
+});
+
+console.log(result); 
+
